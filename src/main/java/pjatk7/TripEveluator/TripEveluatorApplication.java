c07740f270273a896fe8b3e15d018ed872e69612
Jakub Gwiazda <s20497@pjwstk.edu.pl>
2022-01-19 20:44:24 +0100

    Initialized project with pom.xml dependencies

---
+++ src/main/java/pjatk7/TripEveluator/TripEveluatorApplication.java
@@ -0,0 +1,13 @@
+package pjatk7.TripEveluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripEveluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripEveluatorApplication.class, args);
+	}
+
+}
